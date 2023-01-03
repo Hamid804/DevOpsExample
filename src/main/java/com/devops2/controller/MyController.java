e0a07001ba8e0b308ee2f0a92765a326b626ffd2
Hamid Hussain <hamideraqui0786@gmail.com>
2023-01-03 21:32:11 +0530

    Check compose

--- src/main/java/com/devops2/controller/MyController.java
+++ src/main/java/com/devops2/controller/MyController.java
@@ -11,7 +11,7 @@ public class MyController {
 	
 	@GetMapping("/")
 	public String welcome() {
-		return "Welcome to rest api...it's working...docker-compose...updated";
+		return "Welcome to rest api...it's working...docker-compose...updated..new";
 	}
 	
 	@GetMapping("/about")
