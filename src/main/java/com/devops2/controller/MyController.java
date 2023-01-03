439bef5563d091c902804798513e1687498c98f8
Hamid Hussain <hamideraqui0786@gmail.com>
2023-01-04 00:13:47 +0530

    initial check

--- src/main/java/com/devops2/controller/MyController.java
+++ src/main/java/com/devops2/controller/MyController.java
@@ -11,7 +11,7 @@ public class MyController {
 	
 	@GetMapping("/")
 	public String welcome() {
-		return "Welcome to rest api...it's working...Initial...dangling deleted...";
+		return "Welcome to rest api...it's working...Initial...dangling update";
 	}
 	
 	@GetMapping("/about")
